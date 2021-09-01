47075ec9fe14ac737ea39b96f8c1c567c3cc9e54
Basma Mostafa <57597830+basmaamostafa@users.noreply.github.com>
2021-09-01 13:13:08 +0200

    .

--- src/components/Screens/Profile/OrderDetailes.jsx
+++ src/components/Screens/Profile/OrderDetailes.jsx
@@ -5,7 +5,7 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { axios } from "../../../Config/Axios";
 import ParallaxHeader from "@fabfit/react-native-parallax-header";
-import { Alert, View } from "react-native";
+import { Alert, View, Image, TouchableOpacity } from "react-native";
 import { CardEcomOne } from "react-native-card-ui";
 import { ScrollView } from "react-native-gesture-handler";
 import { Header } from "react-native-elements/dist/header/Header";
@@ -15,21 +15,6 @@ import ProcessingTab from "./Processing";
 import OrderedTab from "./Ordered";
 import { useNavigation, useRoute } from "@react-navigation/native";
 import { scale } from "react-native-size-matters";
-
-const Tab = createMaterialTopTabNavigator();
-
-const DeliveredScreenTab = (props) => {
-  const navigation = useNavigation();
-  return <DeliveredTab {...props} navigation={navigation} />;
-};
-const ProcessingScreenTab = (props) => {
-  const navigation = useNavigation();
-  return <ProcessingTab {...props} navigation={navigation} />;
-};
-const OrderedScreenTab = (props) => {
-  const navigation = useNavigation();
-  return <OrderedTab {...props} navigation={navigation} />;
-};
 
 export default class Order extends Component {
   async componentDidMount() {
@@ -59,7 +44,7 @@ export default class Order extends Component {
             width: "98%",
           }}
           centerComponent={{
-            text: "Order Detailes",
+            text: "Order Details",
             style: { color: "#fff", fontSize: scale(20) },
           }}
           leftComponent={{
@@ -79,6 +64,175 @@ export default class Order extends Component {
           //   },
           // }}
         />
+        <ScrollView
+          contentContainerStyle={{ height: "100%" }}
+          // refreshControl={
+          //   <RefreshControl
+          //     refreshing={this.state.refresh}
+          //     onRefresh={this.onRefresh}
+          //     tintColor="white"
+          //   />
+          // }
+        >
+          <View style={{ marginHorizontal: "5%" }}>
+            <View
+              style={{
+                flexDirection: "row",
+                alignItems: "center",
+                justifyContent: "space-between",
+              }}
+            >
+              <Text style={{ color: "#fff", fontSize: 16 }}>
+                Order No. 1312587
+              </Text>
+              <Text style={{ color: "#ABB4BD", fontSize: 16 }}>05-6-2021</Text>
+            </View>
+            <View
+              style={{
+                flexDirection: "row-reverse",
+                alignItems: "center",
+                justifyContent: "space-between",
+                marginTop: 6,
+                // marginHorizontal: "5%",
+              }}
+            >
+              {/* <Text style={{ color: "#fff", fontSize: 16 }}>
+                Tracking Number
+              </Text>
+              <Text style={{ color: "#ABB4BD", fontSize: 16 }}>052021</Text> */}
+              <Text
+                style={{
+                  color: "#2AA952",
+                  fontSize: 16,
+                  // justifyContent: "flex-end",
+                  // alignItems: "flex-end",
+                }}
+              >
+                Delivered
+              </Text>
+            </View>
+            <Text style={{ color: "#fff", fontSize: 16, marginVertical: 10 }}>
+              3 Items
+            </Text>
+            <View>
+              <View
+                style={{
+                  backgroundColor: "#2A2C36",
+                  width: "100%",
+                  height: 130,
+                  borderRadius: 5,
+                  marginTop: scale(5),
+                  marginBottom: scale(5),
+                  // flex: 1,
+                  flexDirection: "row",
+                }}
+              >
+                <Image
+                  source={require("../../../assets/images/image2.png")}
+                  style={{
+                    height: "100%",
+                    width: scale(110),
+                    borderRadius: 5,
+                    marginBottom: 0,
+                  }}
+                />
+                <View>
+                  <View
+                    style={{
+                      flexDirection: "row",
+                      justifyContent: "space-between",
+                      alignItems: "center",
+                      width: scale(220),
+                      // width: "100%",
+                    }}
+                  >
+                    <Text
+                      style={{
+                        color: "white",
+                        fontSize: 20,
+                        marginLeft: 20,
+                        marginTop: 10,
+                        // width: "50%",
+                      }}
+                    >
+                      {/* {this.props.name} */}
+                      name
+                    </Text>
+                  </View>
+                  <View style={{ flex: 1, flexDirection: "row" }}>
+                    <Text
+                      style={{
+                        color: "#ABB4BD",
+                        fontSize: 15,
+                        marginLeft: 20,
+                        marginTop: 10,
+                      }}
+                    >
+                      Size:
+                    </Text>
+                    <Text
+                      style={{
+                        color: "#fff",
+                        fontSize: 15,
+                        marginLeft: 10,
+                        marginTop: 10,
+                      }}
+                    >
+                      {/* {this.props.size.size} */}L
+                    </Text>
+                  </View>
+                  <View
+                    style={{
+                      // flex: 1,
+                      flexDirection: "row",
+                      marginLeft: 20,
+                      // marginBottom: 15,
+                      // alignItems: "center",
+                      justifyContent: "space-between",
+                    }}
+                  >
+                    <View style={{ flexDirection: "row" }}>
+                      <Text
+                        style={{
+                          color: "#ABB4BD",
+                          fontSize: 15,
+                          // marginLeft: 20,
+                          // marginTop: 10,
+                        }}
+                      >
+                        Units:
+                      </Text>
+                      <Text
+                        style={{
+                          color: "#fff",
+                          fontSize: 15,
+                          marginLeft: 10,
+                          // marginTop: 10,
+                        }}
+                      >
+                        {/* {this.props.size.size} */}L
+                      </Text>
+                    </View>
+                    <Text
+                      style={{
+                        color: "white",
+
+                        fontSize: 18,
+                        // marginLeft: 12,
+                      }}
+                    >
+                      54
+                      {/* {this.props.price
+                    .toFixed(2)
+                    .replace(/\d(?=(\d{3})+\.)/g, "$&,")}{" "} */}
+                      EGP
+                    </Text>
+                  </View>
+                </View>
+              </View>
+            </View>
+          </View>
+        </ScrollView>
       </>
     );
   }
